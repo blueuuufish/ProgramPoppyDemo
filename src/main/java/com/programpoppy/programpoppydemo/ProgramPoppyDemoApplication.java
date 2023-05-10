117d17dc2984b98dd927092b0de35c256e937c09
blueuuufish <1638587762@qq.com>
2023-05-10 08:55:13 +0800

    init commit

---
+++ src/main/java/com/programpoppy/programpoppydemo/ProgramPoppyDemoApplication.java
@@ -0,0 +1,13 @@
+package com.programpoppy.programpoppydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramPoppyDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgramPoppyDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/programpoppy/programpoppydemo/ProgramPoppyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programpoppy.programpoppydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramPoppyDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
