207acde111f324a55c8da1875df00e804525c10d
blueuuufish <1638587762@qq.com>
2024-02-20 12:30:14 +0800

    RedisTemplate

--- programpoppy-Redis/src/test/java/com/programpoppy/programpoppyredis/ProgrampoppyRedisApplicationTests.java
+++ programpoppy-Redis/src/test/java/com/programpoppy/programpoppyredis/ProgrampoppyRedisApplicationTests.java
@@ -1,13 +1,26 @@
 package com.programpoppy.programpoppyredis;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 @SpringBootTest
 class ProgrampoppyRedisApplicationTests {
 
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
     @Test
-    void contextLoads() {
+    void contextLoads()  {
+        redisTemplate.opsForValue().set("name", "test");
+        String name = redisTemplate.opsForValue().get("name");
+        System.out.println(name);
     }
 
 }
